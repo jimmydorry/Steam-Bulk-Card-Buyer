8278a665c113ab78e869085827d38fbfdce7d332
jimmydorry <jimmydorry@users.noreply.github.com>
2014-06-26 08:51:18 +1000

    Update badgebuy.user.js

--- badgebuy.user.js
+++ badgebuy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			Steam Trading Cards Bulk Buyer
 // @namespace		http://www.doctormckay.com/
-// @version			3.0.2
+// @version			3.1.2
 // @description		Provides a button to purchase remaining cards needed for a badge in bulk
 // @match			http://steamcommunity.com/*/gamecards/*
 // @require			http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
@@ -20,9 +20,9 @@ var appid = 0;
 var failures = [];
 
 // Current currency (numerical identifier used by Steam)
-var currency = 1;
+var g_Currency = 1;
 // Detailed information for each currency ID (using information taken from Steam's Javascript source code)
-var currencyInfo =
+var g_CurrencyInfo =
 {
     1: { symbol: "$", separator: "." },
     2: { symbol: "£", separator: "." },
@@ -35,9 +35,9 @@ function formatPrice(price, full)
 {
     if(full)
 	{
-		return currencyInfo[currency].symbol + price.replace(".", currencyInfo[currency].separator);
+		return g_CurrencyInfo[g_Currency].symbol + price.replace(".", g_CurrencyInfo[g_Currency].separator);
 	}
-	return price.replace(".", currencyInfo[currency].separator);
+	return price.replace(".", g_CurrencyInfo[g_Currency].separator);
 }
 
 var items = $('.unowned a'); // Enhanced Steam turns unowned into links
@@ -70,8 +70,13 @@ if(links && $('.unowned').length > 0) {
 	});
 }
 
+var g_CommodityStatus = -1; // -1 = unknown, 0 = not a commodity, 1 = is a commodity
+var g_SessionID;
+
 function updatePrices() {
 	$('#buycardspanel').html('');
+    
+    g_CommodityStatus = -1;
 	
 	for(var i = 0; i < items.length; i++) {
 		var name = getCardName(items[i]);
@@ -99,6 +104,7 @@ function onCardPriceLoaded(data, textStatus) {
 	
 	var title = html.find('title').text();
 	var name = title.substring(title.indexOf('-') + 1);
+	var hashName = title.substring(title.indexOf('Listings for ') + 13);
 	
 	if(data.indexOf('There are no listings for this item.') != -1 && name.indexOf('(Trading Card)') == -1 && name.indexOf('(Foil Trading Card)') == -1) {
 		$.get('/market/listings/753/' + title.substring(title.indexOf('Listings for') + 13, title.indexOf('-')) + '-' + encodeURIComponent(name) + ' (' + ((window.location.href.indexOf('?border=1') != -1) ? 'Foil ' : '') + 'Trading Card)', onCardPriceLoaded)
@@ -110,57 +116,90 @@ function onCardPriceLoaded(data, textStatus) {
 	
 	name = name.replace(' (Trading Card)', '').replace(' (Foil Trading Card)', '').replace(' (Foil)', '');
 	
-	var item = findElementByClass(html, 'div', 'market_listing_row');
-	var price = findElementByClass($(item), 'span', 'market_listing_price_with_fee');
-	var pricenofee = findElementByClass($(item), 'span', 'market_listing_price_without_fee');
+    if(g_CommodityStatus == -1) {
+        g_CommodityStatus = !!data.match(/Market_LoadOrderSpread\(\s?\d+\s?\);/);
+		g_SessionID = data.match(/g_sessionID = ".+";/)[0];
+		g_SessionID = g_SessionID.substring(15, g_SessionID.length - 2);
+    }
 	
-	if(textStatus != 'success' || !item || !price || !pricenofee) {
-		priceElement(name).html('Error');
+	if(!g_CommodityStatus) {
+		var item = findElementByClass(html, 'div', 'market_listing_row');
+		var price = findElementByClass($(item), 'span', 'market_listing_price_with_fee');
+		var pricenofee = findElementByClass($(item), 'span', 'market_listing_price_without_fee');
+		
+		if(textStatus != 'success' || !item || !price || !pricenofee) {
+			priceElement(name).html('Error');
+		} else {
+			var listingID = $(item).attr('id').split('_')[1];
+			
+			// Find out which currency we are dealing with by looking for the valuta symbol
+			$.each(g_CurrencyInfo, function(index) {
+				if($(price).text().indexOf(this.symbol) !== -1)
+				{
+					g_Currency = index;
+				}
+			});
+			
+			// Just translate all commas to dots so we have floating point values
+			var totalPrice = $(price).html().replace(",", ".").replace(/[^0-9.]/g, '');
+			var theirPrice = $(pricenofee).html().replace(",", ".").replace(/[^0-9.]/g, '');
+			
+			if(totalPrice == 'Sold!') {
+				$.get('/market/listings/753/' + title.substring(title.indexOf('Listings for') + 13), onCardPriceLoaded)
+					.fail(function() {
+						priceElement(name).html('Error');
+					});
+				return;
+			}
+			
+			cards.push({listing: listingID, total: totalPrice, theirs: theirPrice, name: name});
+			
+			// Use new currency information
+			priceElement(name).html(formatPrice(totalPrice, true));
+		}
+		
+		if(cards.length == $('.cardprice').length) {
+			var total = 0;
+			for(var i = 0; i < cards.length; i++) {
+				total += parseFloat(cards[i].total);
+			}
+			
+			// Use new currency information
+			$('#buycardspanel').append('<br /><span style="font-weight: bold; display: inline-block; width: ' + $('.cardname').css('width') + '; padding-right: 10px; text-align: right">Total</span><b>' + g_CurrencyInfo[g_Currency].symbol + '<span id="totalprice">' + formatPrice(total.toFixed(2)) + '</span></b><br /><br /><button type="button" id="buycardsbutton" class="btn_green_white_innerfade btn_medium_wide" style="padding: 10px 20px; margin-left: ' + ($('.cardname').css('width').replace('px', '') / 2) + 'px">PURCHASE</button>');
+			$('#buycardsbutton').click(function() {
+				failures = [];
+				$('#buycardsbutton').hide();
+				buyCard();
+			});
+		}
 	} else {
-		var pos = data.indexOf('g_sessionID = "') + 15;
-		var pos2 = data.indexOf('"', pos);
-		var sessionID = data.substring(pos, pos2);
-		
-		var listingID = $(item).attr('id').split('_')[1];
-        
-		// Find out which currency we are dealing with by looking for the valuta symbol
-        $.each(currencyInfo, function(index) {
-            if($(price).text().indexOf(this.symbol) !== -1)
-            {
-                currency = index;
-            }
-        });
-		
-		// Just translate all commas to dots so we have floating point values
-		var totalPrice = $(price).html().replace(",", ".").replace(/[^0-9.]/g, '');
-		var theirPrice = $(pricenofee).html().replace(",", ".").replace(/[^0-9.]/g, '');
-		
-		if(totalPrice == 'Sold!') {
-			$.get('/market/listings/753/' + title.substring(title.indexOf('Listings for') + 13), onCardPriceLoaded)
-				.fail(function() {
-					priceElement(name).html('Error');
+		var countryCode = data.match(/g_strCountryCode = "[a-zA-Z0-9]+";/)[0].match(/"[a-zA-Z0-9]+/)[0].substring(1);
+		g_Currency = data.match(/"wallet_currency":\d/)[0].match(/\d/)[0];
+		var nameid = data.match(/Market_LoadOrderSpread\(\s?\d+\s?\);/)[0].match(/\d+/)[0];
+		$.get('/market/itemordershistogram', {country: countryCode, language: 'english', currency: g_Currency, "item_nameid": nameid}, function(json) {
+			if(!json.success) {
+				priceElement(name).text('Error');
+				return;
+			}
+			
+			var price = (parseInt(json.lowest_sell_order) / 100).toFixed(2);
+			
+			cards.push({nameid: nameid, name: name, hashName: hashName, price: json.lowest_sell_order});
+			priceElement(name).html(formatPrice(price, true));
+			
+			if(cards.length == $('.cardprice').length) {
+				var total = 0;
+				for(var i = 0; i < cards.length; i++) {
+					total += parseFloat(cards[i].price / 100);
+				}
+				
+				$('#buycardspanel').append('<br /><span style="font-weight: bold; display: inline-block; width: ' + $('.cardname').css('width') + '; padding-right: 10px; text-align: right">Total</span><b>' + g_CurrencyInfo[g_Currency].symbol + '<span id="totalprice">' + formatPrice(total.toFixed(2)) + '</span></b><br /><br /><button type="button" id="buycardsbutton" class="btn_green_white_innerfade btn_medium_wide" style="padding: 10px 20px; margin-left: ' + ($('.cardname').css('width').replace('px', '') / 2) + 'px">PLACE ORDERS</button>');
+				$('#buycardsbutton').click(function() {
+					failures = [];
+					$('#buycardsbutton').hide();
+					placeBuyOrder();
 				});
-			return;
-		}
-		
-		cards.push({session: sessionID, listing: listingID, total: totalPrice, theirs: theirPrice, name: name});
-		
-		// Use new currency information
-		priceElement(name).html(formatPrice(totalPrice, true));
-	}
-	
-	if(cards.length == $('.cardprice').length) {
-		var total = 0;
-		for(var i = 0; i < cards.length; i++) {
-			total += parseFloat(cards[i].total);
-		}
-		
-		// Use new currency information
-		$('#buycardspanel').append('<br /><span style="font-weight: bold; display: inline-block; width: ' + $('.cardname').css('width') + '; padding-right: 10px; text-align: right">Total</span><b>' + currencyInfo[currency].symbol + '<span id="totalprice">' + formatPrice(total.toFixed(2)) + '</span></b><br /><br /><button type="button" id="buycardsbutton" class="btn_green_white_innerfade btn_medium_wide" style="padding: 10px 20px 10px 20px; margin-left: ' + ($('.cardname').css('width').replace('px', '') / 2) + 'px">PURCHASE</button>');
-		$('#buycardsbutton').click(function() {
-			failures = [];
-			$('#buycardsbutton').hide();
-			buyCard();
+			}
 		});
 	}
 }
@@ -208,7 +247,7 @@ function buyCard() {
 	
 	priceElement(item.name)[0].innerHTML += ' - Purchasing...';
 	// Use new currency indicator
-	$.post('https://steamcommunity.com/market/buylisting/' + item.listing, {sessionid: item.session, currency: currency, subtotal: Math.round(item.theirs * 100), fee: Math.round((item.total * 100) - (item.theirs * 100)), total: Math.round(item.total * 100)}, function(data, textStatus) {
+	$.post('https://steamcommunity.com/market/buylisting/' + item.listing, {sessionid: g_SessionID, currency: g_Currency, subtotal: Math.round(item.theirs * 100), fee: Math.round((item.total * 100) - (item.theirs * 100)), total: Math.round(item.total * 100)}, function(data, textStatus) {
 		if(textStatus != 'success' || !data || !data.wallet_info || !data.wallet_info.success) {
 			priceElement(item.name).html('Failure');
 			failures.push(item.name);
@@ -233,6 +272,76 @@ function buyCard() {
 		
 		cards.splice(0, 1);
 		buyCard();
+	});
+}
+
+function placeBuyOrder() {
+	var card = cards.splice(0, 1)[0];
+	if(!card) {
+		return;
+	}
+	
+	priceElement(card.name)[0].innerHTML += ' - Placing buy order...';
+	
+	$.post('https://steamcommunity.com/market/createbuyorder/', {sessionid: g_SessionID, currency: g_Currency, appid: 753, market_hash_name: card.hashName, price_total: card.price, quantity: 1}, function(json) {
+		placeBuyOrder();
+		
+		if(!json.success) {
+			priceElement(card.name).text('Failure');
+			return;
+		}
+		
+		card.orderid = json.buy_orderid;
+		card.checks = 0;
+		
+		priceElement(card.name).text(priceElement(card.name).text().replace('Placing buy order', 'Waiting'));
+		checkOrderStatus(card);
+	});
+}
+
+function checkOrderStatus(card) {
+	priceElement(card.name)[0].innerHTML += '.';
+	
+	$.get('/market/getbuyorderstatus/', {sessionid: g_SessionID, buy_orderid: card.orderid}, function(json) {
+		if(!json.success) {
+			setTimeout(function() {
+				checkOrderStatus(card);
+			}, 500);
+			return;
+		}
+		
+		if(json.purchases.length) {
+			if(json.purchases[0].price_total < card.price) {
+				decrementTotal((card.price - json.purchases[0].price_total) / 100);
+			}
+			
+			priceElement(card.name).text(formatPrice((json.purchases[0].price_total / 100).toFixed(2), true) + ' - Purchased');
+			return;
+		}
+		
+		if(!json.purchases.length) {
+			card.checks++;
+			if(card.checks >= 10) {
+				cancelBuyOrder(card.orderid);
+				priceElement(card.name).text('Order unfulfilled');
+				decrementTotal(card.price / 100);
+				return;
+			}
+		}
+		
+		setTimeout(function() {
+			checkOrderStatus(card);
+		}, 500);
+	});
+}
+
+function cancelBuyOrder(orderid) {
+	$.post('/market/cancelbuyorder/', {sessionid: g_SessionID, buy_orderid: orderid}, function(json) {
+		if(!json.success) {
+			setTimeout(function() {
+				cancelBuyOrder(orderid);
+			}, 500);
+		}
 	});
 }
 
